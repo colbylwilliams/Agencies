30fa18ff8848cc18190866b2daeb9b5573e96ce4
Nate Rickard <nate.rickard@microsoft.com>
2017-03-21 16:07:41 -0400

    [droid] testing Face binding api

--- Agencies/Agencies.Droid/MainActivity.cs
+++ Agencies/Agencies.Droid/MainActivity.cs
@@ -1,14 +1,13 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using Com.Microsoft.Projectoxford.Face;
 
 namespace Agencies.Droid
 {
 	[Activity (Label = "Agencies", MainLauncher = true, Icon = "@mipmap/icon")]
 	public class MainActivity : Activity
 	{
-		int count = 1;
-
 		protected override void OnCreate (Bundle savedInstanceState)
 		{
 			base.OnCreate (savedInstanceState);
@@ -16,12 +15,7 @@ namespace Agencies.Droid
 			// Set our view from the "main" layout resource
 			SetContentView (Resource.Layout.Main);
 
-			// Get our button from the layout resource,
-			// and attach an event to it
-			Button button = FindViewById<Button> (Resource.Id.myButton);
-
-			button.Click += delegate { button.Text = $"{count++} clicks!"; };
+			FaceServiceRestClient client = new FaceServiceRestClient (Keys.CognitiveServices.FaceApi.SubscriptionKey);
 		}
 	}
 }
-
