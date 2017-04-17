e6e82efd04dbba67026bcb05585d1739ebdd0530
Nate Rickard <nate.rickard@microsoft.com>
2017-04-17 12:29:50 -0400

    [ios] fixed .designer file for AgenciesBotController

--- Agencies/Agencies.iOS/AgenciesBotViewController.designer.cs
+++ Agencies/Agencies.iOS/AgenciesBotViewController.designer.cs
@@ -1,20 +1,32 @@
 // WARNING
 //
-// This file has been generated automatically by Xamarin Studio to store outlets and
-// actions made in the UI designer. If it is removed, they will be lost.
-// Manual changes to this file may not be handled correctly.
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
 //
 using Foundation;
+using System;
 using System.CodeDom.Compiler;
 
 namespace Agencies.iOS
 {
-	[Register ("AgenciesBotViewController")]
-	partial class AgenciesBotViewController
-	{
-		
-		void ReleaseDesignerOutlets ()
-		{
-		}
-	}
+    [Register ("AgenciesBotViewController")]
+    partial class AgenciesBotViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIBarButtonItem PeopleButton { get; set; }
+
+        [Action ("PeopleAction:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void PeopleAction (UIKit.UIBarButtonItem sender);
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (PeopleButton != null) {
+                PeopleButton.Dispose ();
+                PeopleButton = null;
+            }
+        }
+    }
 }
