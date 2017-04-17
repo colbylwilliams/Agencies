07425ea04b9e487ea315b03b4e3dde255bfb158d
Colby Williams <colbylwilliams@gmail.com>
2017-04-17 15:36:02 -0400

    [service] get app configuraion from service

--- Agencies/Agencies.AppService/Dialogs/SimpleQnAMakerDialog.cs
+++ Agencies/Agencies.AppService/Dialogs/SimpleQnAMakerDialog.cs
@@ -28,6 +28,17 @@ namespace Agencies.AppService.Dialogs
 	[Serializable]
 	public class FAQDialog : IDialog<object>
 	{
+		private const string HeroCard = "Hero card";
+		private const string ThumbnailCard = "Thumbnail card";
+		private const string ReceiptCard = "Receipt card";
+		private const string SigninCard = "Sign-in card";
+		private const string AnimationCard = "Animation card";
+		private const string VideoCard = "Video card";
+		private const string AudioCard = "Audio card";
+
+		private IEnumerable<string> options = new List<string> { HeroCard, ThumbnailCard, ReceiptCard, SigninCard, AnimationCard, VideoCard, AudioCard };
+
+
 		public Task StartAsync(IDialogContext context)
 		{
 			context.Wait(MessageReceivedAsync);
@@ -39,147 +50,28 @@ namespace Agencies.AppService.Dialogs
 		{
 			var message = await argument as Activity;
 
-			if (message.Text == "HeroCard")
+			//Call the QnAMaker Dialog if the message is a question.
+			if (IsQuestion(message.Text))
 			{
-				Activity replyToConversation = message.CreateReply("Should go to conversation, with a hero card");
-				replyToConversation.Recipient = message.From;
-				replyToConversation.Type = "message";
-				replyToConversation.Attachments = new List<Attachment>();
-				List<CardImage> cardImages = new List<CardImage>();
-				cardImages.Add(new CardImage(url: "https://<ImageUrl1>"));
-				cardImages.Add(new CardImage(url: "https://<ImageUrl2>"));
-				List<CardAction> cardButtons = new List<CardAction>();
-				CardAction plButton = new CardAction()
-				{
-					Value = "https://en.wikipedia.org/wiki/Pig_Latin",
-					Type = "openUrl",
-					Title = "WikiPedia Page"
-				};
-				cardButtons.Add(plButton);
-				HeroCard plCard = new HeroCard()
-				{
-					Title = "I'm a hero card",
-					Subtitle = "Pig Latin Wikipedia Page",
-					Images = cardImages,
-					Buttons = cardButtons
-				};
-				Attachment plAttachment = plCard.ToAttachment();
-				replyToConversation.Attachments.Add(plAttachment);
+				await context.Forward(new SimpleQnAMakerDialog(), AfterQnA, message, CancellationToken.None);
 
-				await context.PostAsync(replyToConversation);
-
+				context.Wait(MessageReceivedAsync);
 			}
-			else if (message.Text == "Thumbnail")
+			else if(options.Any(o => o.Equals(message.Text, StringComparison.Ordinal)))
 			{
-				Activity replyToConversation = message.CreateReply("Should go to conversation, with a thumbnail card");
-				replyToConversation.Recipient = message.From;
-				replyToConversation.Type = "message";
-				replyToConversation.Attachments = new List<Attachment>();
-				List<CardImage> cardImages = new List<CardImage>();
-				cardImages.Add(new CardImage(url: "https://<ImageUrl1>"));
-				List<CardAction> cardButtons = new List<CardAction>();
-				CardAction plButton = new CardAction()
-				{
-					Value = "https://en.wikipedia.org/wiki/Pig_Latin",
-					Type = "openUrl",
-					Title = "WikiPedia Page"
-				};
-				cardButtons.Add(plButton);
-				ThumbnailCard plCard = new ThumbnailCard()
-				{
-					Title = "I'm a thumbnail card",
-					Subtitle = "Pig Latin Wikipedia Page",
-					Images = cardImages,
-					Buttons = cardButtons
-				};
-				Attachment plAttachment = plCard.ToAttachment();
-				replyToConversation.Attachments.Add(plAttachment);
-				await context.PostAsync(replyToConversation);
-
-			}
-			else if (message.Text == "Receipt")
-			{
-				Activity replyToConversation = message.CreateReply("Receipt card");
-				replyToConversation.Recipient = message.From;
-				replyToConversation.Type = "message";
-				replyToConversation.Attachments = new List<Attachment>();
-				List<CardImage> cardImages = new List<CardImage>();
-				cardImages.Add(new CardImage(url: "https://<ImageUrl1>"));
-				List<CardAction> cardButtons = new List<CardAction>();
-				CardAction plButton = new CardAction()
-				{
-					Value = "https://en.wikipedia.org/wiki/Pig_Latin",
-					Type = "openUrl",
-					Title = "WikiPedia Page"
-				};
-				cardButtons.Add(plButton);
-				ReceiptItem lineItem1 = new ReceiptItem()
-				{
-					Title = "Pork Shoulder",
-					Subtitle = "8 lbs",
-					Text = null,
-					Image = new CardImage(url: "https://<ImageUrl1>"),
-					Price = "16.25",
-					Quantity = "1",
-					Tap = null
-				};
-				ReceiptItem lineItem2 = new ReceiptItem()
-				{
-					Title = "Bacon",
-					Subtitle = "5 lbs",
-					Text = null,
-					Image = new CardImage(url: "https://<ImageUrl2>"),
-					Price = "34.50",
-					Quantity = "2",
-					Tap = null
-				};
-				List<ReceiptItem> receiptList = new List<ReceiptItem>();
-				receiptList.Add(lineItem1);
-				receiptList.Add(lineItem2);
-				ReceiptCard plCard = new ReceiptCard()
-				{
-					Title = "I'm a receipt card, isn't this bacon expensive?",
-					Buttons = cardButtons,
-					Items = receiptList,
-					Total = "275.25",
-					Tax = "27.52"
-				};
-				Attachment plAttachment = plCard.ToAttachment();
-				replyToConversation.Attachments.Add(plAttachment);
-
-				await context.PostAsync(replyToConversation);
-			}
-			else if (message.Text == "Signin")
-			{
-				Activity replyToConversation = message.CreateReply("Should go to conversation, sign-in card");
-				replyToConversation.Recipient = message.From;
-				replyToConversation.Type = "message";
-				replyToConversation.Attachments = new List<Attachment>();
-				List<CardAction> cardButtons = new List<CardAction>();
-				CardAction plButton = new CardAction()
-				{
-					Value = "https://<OAuthSignInURL>",
-					Type = "signin",
-					Title = "Connect"
-				};
-				cardButtons.Add(plButton);
-				SigninCard plCard = new SigninCard("You need to authorize me", new List<CardAction> { plButton });
-				Attachment plAttachment = plCard.ToAttachment();
-				replyToConversation.Attachments.Add(plAttachment);
-				await context.PostAsync(replyToConversation);
+				await DisplaySelectedCard(context, message.Text);
 			}
 			else
 			{
-				//Call the QnAMaker Dialog if the message is a question.
-				if (IsQuestion(message.Text))
-				{
-					await context.Forward(new SimpleQnAMakerDialog(), AfterQnA, message, CancellationToken.None);
-				}
-				else
-					await context.PostAsync("This doesn't look like a question.");
+				PromptDialog.Choice<string>(
+					context,
+					DisplaySelectedCard,
+					options,
+					"What card would like to test?",
+					"Ooops, what you wrote is not a valid option, please try again",
+					3,
+					PromptStyle.PerLine);
 			}
-
-			context.Wait(MessageReceivedAsync);
 		}
 
 		//Callback, after the QnAMaker Dialog returns a result.
@@ -204,6 +96,209 @@ namespace Agencies.AppService.Dialogs
 				return true;
 			else
 				return false;
+		}
+
+
+		public async Task DisplaySelectedCard(IDialogContext context, string selectedCard)
+		{
+			var message = context.MakeMessage();
+
+			var attachment = GetSelectedCard(selectedCard);
+			message.Attachments.Add(attachment);
+
+			await context.PostAsync(message);
+
+			context.Wait(this.MessageReceivedAsync);
+		}
+
+
+		public async Task DisplaySelectedCard(IDialogContext context, IAwaitable<string> result)
+		{
+			var selectedCard = await result;
+
+			var message = context.MakeMessage();
+
+			var attachment = GetSelectedCard(selectedCard);
+			message.Attachments.Add(attachment);
+
+			await context.PostAsync(message);
+
+			context.Wait(this.MessageReceivedAsync);
+		}
+
+		private static Attachment GetSelectedCard(string selectedCard)
+		{
+			switch (selectedCard)
+			{
+				case HeroCard:
+					return GetHeroCard();
+				case ThumbnailCard:
+					return GetThumbnailCard();
+				case ReceiptCard:
+					return GetReceiptCard();
+				case SigninCard:
+					return GetSigninCard();
+				case AnimationCard:
+					return GetAnimationCard();
+				case VideoCard:
+					return GetVideoCard();
+				case AudioCard:
+					return GetAudioCard();
+
+				default:
+					return GetHeroCard();
+			}
+		}
+
+		private static Attachment GetHeroCard()
+		{
+			var heroCard = new HeroCard
+			{
+				Title = "BotFramework Hero Card",
+				Subtitle = "Your bots — wherever your users are talking",
+				Text = "Build and connect intelligent bots to interact with your users naturally wherever they are, from text/sms to Skype, Slack, Office 365 mail and other popular services.",
+				Images = new List<CardImage> { new CardImage("https://sec.ch9.ms/ch9/7ff5/e07cfef0-aa3b-40bb-9baa-7c9ef8ff7ff5/buildreactionbotframework_960.jpg") },
+				Buttons = new List<CardAction> { new CardAction(ActionTypes.OpenUrl, "Get Started", value: "https://docs.botframework.com/en-us/") }
+			};
+
+			return heroCard.ToAttachment();
+		}
+
+		private static Attachment GetThumbnailCard()
+		{
+			var heroCard = new ThumbnailCard
+			{
+				Title = "BotFramework Thumbnail Card",
+				Subtitle = "Your bots — wherever your users are talking",
+				Text = "Build and connect intelligent bots to interact with your users naturally wherever they are, from text/sms to Skype, Slack, Office 365 mail and other popular services.",
+				Images = new List<CardImage> { new CardImage("https://sec.ch9.ms/ch9/7ff5/e07cfef0-aa3b-40bb-9baa-7c9ef8ff7ff5/buildreactionbotframework_960.jpg") },
+				Buttons = new List<CardAction> { new CardAction(ActionTypes.OpenUrl, "Get Started", value: "https://docs.botframework.com/en-us/") }
+			};
+
+			return heroCard.ToAttachment();
+		}
+
+		private static Attachment GetReceiptCard()
+		{
+			var receiptCard = new ReceiptCard
+			{
+				Title = "John Doe",
+				Facts = new List<Fact> { new Fact("Order Number", "1234"), new Fact("Payment Method", "VISA 5555-****") },
+				Items = new List<ReceiptItem>
+				{
+					new ReceiptItem("Data Transfer", price: "$ 38.45", quantity: "368", image: new CardImage(url: "https://github.com/amido/azure-vector-icons/raw/master/renders/traffic-manager.png")),
+					new ReceiptItem("App Service", price: "$ 45.00", quantity: "720", image: new CardImage(url: "https://github.com/amido/azure-vector-icons/raw/master/renders/cloud-service.png")),
+				},
+				Tax = "$ 7.50",
+				Total = "$ 90.95",
+				Buttons = new List<CardAction>
+				{
+					new CardAction(
+						ActionTypes.OpenUrl,
+						"More information",
+						"https://account.windowsazure.com/content/6.10.1.38-.8225.160809-1618/aux-pre/images/offer-icon-freetrial.png",
+						"https://azure.microsoft.com/en-us/pricing/")
+				}
+			};
+
+			return receiptCard.ToAttachment();
+		}
+
+		private static Attachment GetSigninCard()
+		{
+			var signinCard = new SigninCard
+			{
+				Text = "BotFramework Sign-in Card",
+				Buttons = new List<CardAction> { new CardAction(ActionTypes.Signin, "Sign-in", value: "https://login.microsoftonline.com/") }
+			};
+
+			return signinCard.ToAttachment();
+		}
+
+		private static Attachment GetAnimationCard()
+		{
+			var animationCard = new AnimationCard
+			{
+				Title = "Microsoft Bot Framework",
+				Subtitle = "Animation Card",
+				Image = new ThumbnailUrl
+				{
+					Url = "https://docs.botframework.com/en-us/images/faq-overview/botframework_overview_july.png"
+				},
+				Media = new List<MediaUrl>
+				{
+					new MediaUrl()
+					{
+						Url = "http://i.giphy.com/Ki55RUbOV5njy.gif"
+					}
+				}
+			};
+
+			return animationCard.ToAttachment();
+		}
+
+		private static Attachment GetVideoCard()
+		{
+			var videoCard = new VideoCard
+			{
+				Title = "Big Buck Bunny",
+				Subtitle = "by the Blender Institute",
+				Text = "Big Buck Bunny (code-named Peach) is a short computer-animated comedy film by the Blender Institute, part of the Blender Foundation. Like the foundation's previous film Elephants Dream, the film was made using Blender, a free software application for animation made by the same foundation. It was released as an open-source film under Creative Commons License Attribution 3.0.",
+				Image = new ThumbnailUrl
+				{
+					Url = "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c5/Big_buck_bunny_poster_big.jpg/220px-Big_buck_bunny_poster_big.jpg"
+				},
+				Media = new List<MediaUrl>
+				{
+					new MediaUrl()
+					{
+						Url = "http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4"
+					}
+				},
+				Buttons = new List<CardAction>
+				{
+					new CardAction()
+					{
+						Title = "Learn More",
+						Type = ActionTypes.OpenUrl,
+						Value = "https://peach.blender.org/"
+					}
+				}
+			};
+
+			return videoCard.ToAttachment();
+		}
+
+		private static Attachment GetAudioCard()
+		{
+			var audioCard = new AudioCard
+			{
+				Title = "I am your father",
+				Subtitle = "Star Wars: Episode V - The Empire Strikes Back",
+				Text = "The Empire Strikes Back (also known as Star Wars: Episode V – The Empire Strikes Back) is a 1980 American epic space opera film directed by Irvin Kershner. Leigh Brackett and Lawrence Kasdan wrote the screenplay, with George Lucas writing the film's story and serving as executive producer. The second installment in the original Star Wars trilogy, it was produced by Gary Kurtz for Lucasfilm Ltd. and stars Mark Hamill, Harrison Ford, Carrie Fisher, Billy Dee Williams, Anthony Daniels, David Prowse, Kenny Baker, Peter Mayhew and Frank Oz.",
+				Image = new ThumbnailUrl
+				{
+					Url = "https://upload.wikimedia.org/wikipedia/en/3/3c/SW_-_Empire_Strikes_Back.jpg"
+				},
+				Media = new List<MediaUrl>
+				{
+					new MediaUrl()
+					{
+						Url = "http://www.wavlist.com/movies/004/father.wav"
+					}
+				},
+				Buttons = new List<CardAction>
+				{
+					new CardAction()
+					{
+						Title = "Read More",
+						Type = ActionTypes.OpenUrl,
+						Value = "https://en.wikipedia.org/wiki/The_Empire_Strikes_Back"
+					}
+				}
+			};
+
+			return audioCard.ToAttachment();
 		}
 	}
 }
