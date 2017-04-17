7fb39a640195862a638503fd23bc0500ca85fa8d
Nate Rickard <nate.rickard@microsoft.com>
2017-04-17 12:20:17 -0400

    [ios] added AgenciesBotController

---
+++ Agencies/Agencies.iOS/AgenciesBotViewController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Foundation;
+using NomadCode.BotFramework.iOS;
+using UIKit;
+
+namespace Agencies.iOS
+{
+    public partial class AgenciesBotViewController : BotViewController
+    {
+        [Export ("initWithCoder:")]
+        public AgenciesBotViewController (NSCoder coder) : base (coder)
+        {
+        }
+
+
+        public AgenciesBotViewController (IntPtr handle) : base (handle)
+        {
+        }
+
+
+        public override void ViewDidLoad ()
+        {
+            base.ViewDidLoad ();
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+
+        public override void DidReceiveMemoryWarning ()
+        {
+            base.DidReceiveMemoryWarning ();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
