6f9104432e42328923ef6f57ae6f49c9dfe4117e
Nate Rickard <nate.rickard@microsoft.com>
2017-04-24 23:01:01 -0400

    FaceClient Grouping methods refined (and working)

--- Agencies/Agencies.Shared/FaceClient.cs
+++ Agencies/Agencies.Shared/FaceClient.cs
@@ -35,49 +35,63 @@ namespace Agencies.Shared
         }
 
 
-        public Task<List<PersonGroup>> GetGroups ()
+        public Task<List<PersonGroup>> GetGroups (bool forceRefresh = false)
         {
-#if __IOS__
-            var tcs = new TaskCompletionSource<List<PersonGroup>> ();
-
-            if (Groups.Count == 0)
+            try
             {
-                Client.ListPersonGroupsWithCompletion ((groups, error) =>
+#if __IOS__
+                if (Groups.Count == 0 || forceRefresh)
                 {
-                    if (!FailTaskIfErrored (tcs, error))
+                    var tcs = new TaskCompletionSource<List<PersonGroup>> ();
+
+                    Client.ListPersonGroupsWithCompletion ((groups, error) =>
                     {
+                        FailTaskIfErrored (tcs, error);
+
                         Groups = new List<PersonGroup> (
                             groups.Select (g => new PersonGroup
                             {
                                 Id = g.PersonGroupId,
-                                Name = g.Name,
-                                UserData = g.UserData
+                                Name = g.Name
+                                // Dealing with a binding issue on UserData - since it's populated via a JSON NSDict, the value will be NSNull, but we're sending a Selector to it assuming it's an NSString
+                                //UserData = g.UserData
                             })
                         );
 
                         tcs.SetResult (Groups);
-                    }
-                }).Resume ();
-            }
+                    }).Resume ();
 
-            return tcs.Task;
+                    return tcs.Task;
+                }
+
+                return Task.FromResult (Groups);
 #elif __ANDROID__
             var groups = Client.ListPersonGroups ();
 
             return Task.FromResult (Groups);
 #endif
+            }
+            catch (Exception ex)
+            {
+                Log.Error (ex.Message);
+                throw;
+            }
         }
 
 
-        public Task CreatePersonGroup (string personGroupId, string groupName, string userData = null)
+        public Task<PersonGroup> CreatePersonGroup (string groupName, string userData = null)
         {
-#if __IOS__
-            var tcs = new TaskCompletionSource<bool> ();
-
-            Client.CreatePersonGroupWithId (personGroupId, groupName, userData, error =>
+            try
             {
-                if (!FailTaskIfErrored (tcs, error))
+#if __IOS__
+                var tcs = new TaskCompletionSource<PersonGroup> ();
+
+                var personGroupId = Guid.NewGuid ().ToString ();
+
+                Client.CreatePersonGroupWithId (personGroupId, groupName, userData, error =>
                 {
+                    FailTaskIfErrored (tcs, error);
+
                     var group = new PersonGroup
                     {
                         Name = groupName,
@@ -87,79 +101,95 @@ namespace Agencies.Shared
 
                     Groups.Add (group);
 
-                    tcs.SetResult (true);
-                }
-            }).Resume ();
+                    tcs.SetResult (group);
+                }).Resume ();
 
-            return tcs.Task;
+                return tcs.Task;
 
 #elif __ANDROID__
-            Client.CreatePersonGroup (personGroupId, groupName, userData);
+	            Client.CreatePersonGroup (personGroupId, groupName, userData);
 
-            return Task.FromResult (true);
+	            return Task.FromResult (null);
 #endif
+            }
+            catch (Exception ex)
+            {
+                Log.Error (ex.Message);
+                throw;
+            }
         }
 
 
-        public Task UpdatePersonGroup (PersonGroup personGroup, string groupName)
+        public Task UpdatePersonGroup (PersonGroup personGroup, string groupName, string userData = null)
         {
-#if __IOS__
-            var tcs = new TaskCompletionSource<bool> ();
-
-            Client.UpdatePersonGroupWithPersonGroupId (personGroup.Id, groupName, personGroup.UserData, error =>
+            try
             {
-                if (!FailTaskIfErrored (tcs, error))
+#if __IOS__
+                var tcs = new TaskCompletionSource<bool> ();
+
+                Client.UpdatePersonGroupWithPersonGroupId (personGroup.Id, groupName, userData, error =>
                 {
+                    FailTaskIfErrored (tcs, error);
+
                     personGroup.Name = groupName;
+                    personGroup.UserData = userData;
 
                     tcs.SetResult (true);
-                }
-            }).Resume ();
+                }).Resume ();
 
-            return tcs.Task;
+                return tcs.Task;
 
 #elif __ANDROID__
-            Client.CreatePersonGroup (personGroup.Id, groupName, personGroup.UserData);
+	            Client.CreatePersonGroup (personGroup.Id, groupName, personGroup.UserData);
 
-            personGroup.Name = groupName;
+	            personGroup.Name = groupName;
 
-            return Task.FromResult (true);
+	            return Task.FromResult (true);
 #endif
+            }
+            catch (Exception ex)
+            {
+                Log.Error (ex.Message);
+                throw;
+            }
         }
 
 
         public Task TrainGroup (PersonGroup personGroup)
         {
-#if __IOS__
-            var tcs = new TaskCompletionSource<bool> ();
-
-            Client.TrainPersonGroupWithPersonGroupId (personGroup.Id, error =>
+            try
             {
-                if (!FailTaskIfErrored (tcs, error))
-                {
-                    tcs.SetResult (true);
-                }
-            }).Resume ();
+#if __IOS__
+                var tcs = new TaskCompletionSource<bool> ();
 
-            return tcs.Task;
+                Client.TrainPersonGroupWithPersonGroupId (personGroup.Id, error =>
+                {
+                    FailTaskIfErrored (tcs, error);
+
+                    tcs.SetResult (true);
+
+                }).Resume ();
+
+                return tcs.Task;
 #elif __ANDROID__
-            return Task.FromResult (true);
+                return Task.FromResult (true);
 #endif
+            }
+            catch (Exception ex)
+            {
+                Log.Error (ex.Message);
+                throw;
+            }
         }
 
 
 #if __IOS__
-        bool FailTaskIfErrored<T> (TaskCompletionSource<T> tcs, NSError error, T result = default (T))
+        void FailTaskIfErrored<T> (TaskCompletionSource<T> tcs, NSError error)
         {
             if (error != null)
             {
                 tcs.SetException (new Exception (error.Description));
-                tcs.SetResult (result);
-
-                return true;
             }
-
-            return false;
         }
 #endif
     }
