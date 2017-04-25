a4fbb023acf6b6e43fddd79344b8c75857607775
Nate Rickard <nate.rickard@microsoft.com>
2017-04-25 16:38:28 -0400

    [ios] added picker and image helpers

--- Agencies/Agencies.iOS/Utilities/CommonUtil.cs
+++ Agencies/Agencies.iOS/Utilities/CommonUtil.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Drawing;
 using System.Threading.Tasks;
+using CoreGraphics;
 using Foundation;
 using MBProgressHUD;
 using UIKit;
@@ -10,15 +12,15 @@ namespace Agencies.iOS
     {
         static MTMBProgressHUD currentHud;
 
-		public static void ShowSimpleAlert (this UIViewController vc, string message, string title = "Hint", string okText = "Ok")
-		{
-			vc.HideHUD ();
+        public static void ShowSimpleAlert (this UIViewController vc, string message, string title = "Hint", string okText = "Ok")
+        {
+            vc.HideHUD ();
 
-			var alertController = UIAlertController.Create (title, message, UIAlertControllerStyle.Alert);
-			alertController.AddAction (UIAlertAction.Create (okText, UIAlertActionStyle.Cancel, null));
+            var alertController = UIAlertController.Create (title, message, UIAlertControllerStyle.Alert);
+            alertController.AddAction (UIAlertAction.Create (okText, UIAlertActionStyle.Cancel, null));
 
-			vc.PresentViewController (alertController, true, null);
-		}
+            vc.PresentViewController (alertController, true, null);
+        }
 
 
         public static Task ShowSimpleAlertWithWait (this UIViewController vc, string message, string title = "Hint", string okText = "Ok")
@@ -38,38 +40,38 @@ namespace Agencies.iOS
 
         public static Task<bool> ShowTwoOptionAlert (this UIViewController vc, string title, string message, string yesText = "Yes", string noText = "No")
         {
-			vc.HideHUD ();
+            vc.HideHUD ();
 
-			var alertController = UIAlertController.Create (title, message, UIAlertControllerStyle.Alert);
-			var tcs = new TaskCompletionSource<bool> ();
+            var alertController = UIAlertController.Create (title, message, UIAlertControllerStyle.Alert);
+            var tcs = new TaskCompletionSource<bool> ();
 
-			alertController.AddAction (UIAlertAction.Create (yesText, UIAlertActionStyle.Default, a => tcs.SetResult (true)));
-			alertController.AddAction (UIAlertAction.Create (noText, UIAlertActionStyle.Cancel, a => tcs.SetResult (false)));
-
-			vc.PresentViewController (alertController, true, null);
-
-			return tcs.Task;
-        }
-
-
-		public static Task<T> ShowActionSheet<T> (this UIViewController vc, string title, string message, params T[] options)
-		{
-			vc.HideHUD ();
-
-			var alertController = UIAlertController.Create (title, message, UIAlertControllerStyle.ActionSheet);
-            var tcs = new TaskCompletionSource<T> ();
-
-            foreach (var option in options)
-            {
-                alertController.AddAction (UIAlertAction.Create (option.ToString (), UIAlertActionStyle.Default, a => tcs.SetResult(option)));
-            }
-
-            alertController.AddAction (UIAlertAction.Create ("Cancel", UIAlertActionStyle.Cancel, a => tcs.SetResult (default(T))));
+            alertController.AddAction (UIAlertAction.Create (yesText, UIAlertActionStyle.Default, a => tcs.SetResult (true)));
+            alertController.AddAction (UIAlertAction.Create (noText, UIAlertActionStyle.Cancel, a => tcs.SetResult (false)));
 
             vc.PresentViewController (alertController, true, null);
 
             return tcs.Task;
-		}
+        }
+
+
+        public static Task<T> ShowActionSheet<T> (this UIViewController vc, string title, string message, params T [] options)
+        {
+            vc.HideHUD ();
+
+            var alertController = UIAlertController.Create (title, message, UIAlertControllerStyle.ActionSheet);
+            var tcs = new TaskCompletionSource<T> ();
+
+            foreach (var option in options)
+            {
+                alertController.AddAction (UIAlertAction.Create (option.ToString (), UIAlertActionStyle.Default, a => tcs.SetResult (option)));
+            }
+
+            alertController.AddAction (UIAlertAction.Create ("Cancel", UIAlertActionStyle.Cancel, a => tcs.SetResult (default (T))));
+
+            vc.PresentViewController (alertController, true, null);
+
+            return tcs.Task;
+        }
 
 
         public static void ShowHUD (this UIViewController vc, string message)
@@ -117,6 +119,46 @@ namespace Agencies.iOS
         }
 
 
+        public static Task<UIImage> ShowPhotoPicker (this UIViewController vc)
+        {
+            return vc.ShowMediaPicker (UIImagePickerControllerSourceType.PhotoLibrary);
+        }
+
+
+        public static Task<UIImage> ShowCameraPicker (this UIViewController vc)
+        {
+            return vc.ShowMediaPicker (UIImagePickerControllerSourceType.Camera);
+        }
+
+
+        public static Task<UIImage> ShowMediaPicker (this UIViewController vc, UIImagePickerControllerSourceType sourceType, bool allowEditing = true)
+        {
+            var tcs = new TaskCompletionSource<UIImage> ();
+
+            var picker = new UIImagePickerController
+            {
+                SourceType = sourceType,
+                MediaTypes = UIImagePickerController.AvailableMediaTypes (sourceType),
+                AllowsEditing = allowEditing
+            };
+
+            picker.FinishedPickingMedia += (sender, e) =>
+            {
+                var image = e.EditedImage ?? e.OriginalImage;
+
+                picker.DismissViewController (true, null);
+
+                tcs.SetResult (image);
+            };
+
+            picker.Canceled += (sender, e) => tcs.SetResult (null);
+
+            vc.PresentViewController (picker, true, null);
+
+            return tcs.Task;
+        }
+
+
         public static Exception ToException (this NSError error)
         {
             if (error != null)
@@ -125,6 +167,28 @@ namespace Agencies.iOS
             }
 
             return new Exception ("No details");
+        }
+
+
+        //public static UIImage Crop (this UIImage image, RectangleF rect)
+        //{
+        //    return image.Crop ((CGRect)rect);
+        //}
+
+
+        public static UIImage Crop (this UIImage image, CGRect rect)
+        {
+            rect = new CGRect (rect.X * image.CurrentScale,
+                               rect.Y * image.CurrentScale,
+                               rect.Width * image.CurrentScale,
+                               rect.Height * image.CurrentScale);
+
+            using (CGImage cr = image.CGImage.WithImageInRect (rect))
+            {
+                var cropped = UIImage.FromImage (cr, image.CurrentScale, image.Orientation);
+
+                return cropped;
+            }
         }
     }
 }
