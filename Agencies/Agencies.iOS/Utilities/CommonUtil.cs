7e07aa5ddd3035c637c93c4995e49258467bacd0
Nate Rickard <nate.rickard@microsoft.com>
2017-04-24 22:57:13 -0400

    [ios] Moar UI helpers and fixes

--- Agencies/Agencies.iOS/Utilities/CommonUtil.cs
+++ Agencies/Agencies.iOS/Utilities/CommonUtil.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 using Foundation;
 using MBProgressHUD;
 using UIKit;
@@ -7,31 +8,81 @@ namespace Agencies.iOS
 {
     public static class UIUtil
     {
-        public static void ShowSimpleDialog (this UIViewController vc, string message)
+        static MTMBProgressHUD currentHud;
+
+		public static void ShowSimpleAlert (this UIViewController vc, string message, string title = "Hint", string okText = "Ok")
+		{
+			vc.HideHUD ();
+
+			var alertController = UIAlertController.Create (title, message, UIAlertControllerStyle.Alert);
+			alertController.AddAction (UIAlertAction.Create (okText, UIAlertActionStyle.Cancel, null));
+
+			vc.PresentViewController (alertController, true, null);
+		}
+
+
+        public static Task ShowSimpleAlertWithWait (this UIViewController vc, string message, string title = "Hint", string okText = "Ok")
         {
             vc.HideHUD ();
 
-            var alertController = UIAlertController.Create ("Hint", message, UIAlertControllerStyle.Alert);
+            var tcs = new TaskCompletionSource<bool> ();
 
-            alertController.ShowViewController (vc, vc);
-            //UIAlertView alartView = new UIAlertView ("Hint", message, null, "Ok");
+            var alertController = UIAlertController.Create (title, message, UIAlertControllerStyle.Alert);
+            alertController.AddAction (UIAlertAction.Create (okText, UIAlertActionStyle.Cancel, a => tcs.SetResult (true)));
+
+            vc.PresentViewController (alertController, true, null);
+
+            return tcs.Task;
         }
 
 
-        static MTMBProgressHUD currentHud;
+        public static Task<bool> ShowTwoOptionAlert (this UIViewController vc, string title, string message, string yesText = "Yes", string noText = "No")
+        {
+			vc.HideHUD ();
+
+			var alertController = UIAlertController.Create (title, message, UIAlertControllerStyle.Alert);
+			var tcs = new TaskCompletionSource<bool> ();
+
+			alertController.AddAction (UIAlertAction.Create (yesText, UIAlertActionStyle.Default, a => tcs.SetResult (true)));
+			alertController.AddAction (UIAlertAction.Create (noText, UIAlertActionStyle.Cancel, a => tcs.SetResult (false)));
+
+			vc.PresentViewController (alertController, true, null);
+
+			return tcs.Task;
+        }
+
+
+		public static Task<T> ShowActionSheet<T> (this UIViewController vc, string title, string message, params T[] options)
+		{
+			vc.HideHUD ();
+
+			var alertController = UIAlertController.Create (title, message, UIAlertControllerStyle.ActionSheet);
+            var tcs = new TaskCompletionSource<T> ();
+
+            foreach (var option in options)
+            {
+                alertController.AddAction (UIAlertAction.Create (option.ToString (), UIAlertActionStyle.Default, a => tcs.SetResult(option)));
+            }
+
+            alertController.AddAction (UIAlertAction.Create ("Cancel", UIAlertActionStyle.Cancel, a => tcs.SetResult (default(T))));
+
+            vc.PresentViewController (alertController, true, null);
+
+            return tcs.Task;
+		}
 
 
         public static void ShowHUD (this UIViewController vc, string message)
         {
             vc.HideHUD ();
 
-            var hud = new MTMBProgressHUD (vc.View)
+            var hud = new MTMBProgressHUD (vc.NavigationController.View)
             {
                 LabelText = message,
                 RemoveFromSuperViewOnHide = true
             };
 
-            vc.View.AddSubview (hud);
+            vc.NavigationController.View.AddSubview (hud);
             hud.Show (true);
 
             currentHud = hud;
@@ -53,14 +104,14 @@ namespace Agencies.iOS
         {
             vc.HideHUD ();
 
-            var hud = new MTMBProgressHUD (vc.View)
+            var hud = new MTMBProgressHUD (vc.NavigationController.View)
             {
                 LabelText = message,
                 Mode = MBProgressHUDMode.Text,
                 RemoveFromSuperViewOnHide = true
             };
 
-            vc.View.AddSubview (hud);
+            vc.NavigationController.View.AddSubview (hud);
             hud.Show (true);
             hud.Hide (true, 1.5);
         }
