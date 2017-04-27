c89d72eee67e54224b5296b717ab676483b856e4
Nate Rickard <nate.rickard@microsoft.com>
2017-04-27 16:09:19 -0400

    added type extensions

---
+++ Agencies/Agencies.Shared/Extensions/TypeExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace Agencies.Shared.Extensions
+{
+    public static class TypeExtensions
+    {
+        public static T ToType<T> (this object obj)
+        {
+            return (T)obj;
+        }
+
+
+        public static T ToType<T> (this object obj, T toType)
+        {
+            return (T)obj;
+        }
+    }
+}
