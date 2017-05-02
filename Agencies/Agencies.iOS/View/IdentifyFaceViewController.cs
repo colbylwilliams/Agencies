27d67a26ef374e890068f80b9bb9e4b2b59d72ca
Nate Rickard <nate.rickard@microsoft.com>
2017-05-02 17:11:38 -0400

    [ios] started Identify controller

---
+++ Agencies/Agencies.iOS/View/IdentifyFaceViewController.cs
@@ -0,0 +1,14 @@
+using System;
+
+using Foundation;
+using UIKit;
+
+namespace Agencies.iOS
+{
+	public partial class IdentifyFaceViewController : UIViewController
+	{
+		public IdentifyFaceViewController (IntPtr handle) : base (handle)
+		{
+		}
+	}
+}
