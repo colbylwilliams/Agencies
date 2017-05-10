62956d494feb0137f8837f61f9ae95a3c6e87e88
Nate Rickard <nate.rickard@microsoft.com>
2017-05-10 02:47:20 -0400

    added IdentificationResult class

---
+++ Agencies/Agencies.Shared/Model/IdentificationResult.cs
@@ -0,0 +1,11 @@
+﻿namespace Agencies.Shared
+{
+    public class IdentificationResult
+    {
+        public Face Face { get; set; }
+
+        public Person Person { get; set; }
+
+        public float Confidence { get; set; }
+    }
+}
