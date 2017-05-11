41e2346aae5e8e8a0d8736b16898cec7f813ab42
Nate Rickard <nate.rickard@microsoft.com>
2017-05-11 19:51:25 -0400

    [ios] Wrapped Face Demo screen in scrollview and added Verification controller

---
+++ Agencies/Agencies.iOS/View/VerificationViewController.cs
@@ -0,0 +1,14 @@
+using System;
+
+using Foundation;
+using UIKit;
+
+namespace Agencies.iOS
+{
+    public partial class VerificationViewController : UIViewController
+    {
+        public VerificationViewController (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
