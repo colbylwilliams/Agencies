89bcedd9c52c10ca87b018e5eaf27bd1286d5c9e
Colby Williams <colbylwilliams@gmail.com>
2017-05-15 10:15:59 -0400

    function auth

--- Agencies/Agencies.Functions/FaceTokenGenerator.cs
+++ Agencies/Agencies.Functions/FaceTokenGenerator.cs
@@ -7,41 +7,61 @@ using System.Threading;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Host;
+using System.Security.Principal;
 
 namespace Agencies.Functions
 {
-    public static class FaceTokenGenerator
-    {
+	public static class FaceTokenGenerator
+	{
 		static string _faceApiSubscription;
 		static string faceApiSubscription => _faceApiSubscription ?? (_faceApiSubscription = Environment.GetEnvironmentVariable ("MS_FaceApiSubscriptionKey"));
 
 
 		[FunctionName ("GetFaceToken")]
-        public static HttpResponseMessage GetFaceToken ([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "tokens/face")]HttpRequestMessage req, TraceWriter log)
-        {
-            if (!Thread.CurrentPrincipal.Identity.IsAuthenticated)
-            {
-                log.Info("Not authenticated");
+		public static HttpResponseMessage GetFaceToken ([HttpTrigger (AuthorizationLevel.Anonymous, "get", Route = "tokens/face")]HttpRequestMessage req, TraceWriter log)
+		{
+			if (!Thread.CurrentPrincipal.Identity.IsAuthenticated)
+			{
+				log.Info ("Not authenticated");
 
-                return req.CreateResponse(HttpStatusCode.Unauthorized);
-            }
+				return req.CreateResponse (HttpStatusCode.Unauthorized);
+			}
 
-            log.Info("Authenticated");
+			log.Info ("Authenticated");
 
-            if ((Thread.CurrentPrincipal as ClaimsPrincipal)?.Identity is ClaimsIdentity identity)
-            {
-                foreach (var claim in identity.Claims)
-                {
-                    log.Info($"claim: {claim.Type} = {claim.Value}");
-                }
-            }
+			if (Thread.CurrentPrincipal is ClaimsPrincipal principal)
+			{
+				log.Info ($"principal:");
 
-            if (string.IsNullOrEmpty(faceApiSubscription))
-            {
-                return req.CreateErrorResponse(HttpStatusCode.NotFound, "Unable to find Face token on server");
-            }
+				foreach (var claim in principal.Claims)
+				{
+					log.Info ($"           claim: {claim.Type} = {claim.Value}");
+				}
 
-            return req.CreateResponse (HttpStatusCode.OK, faceApiSubscription);
-        }
-    }
+				foreach (var identity in principal.Identities)
+				{
+					log.Info ($"identity: {identity.Name}");
+
+					foreach (var claim in identity.Claims)
+					{
+						log.Info ($"          claim: {claim.Type} = {claim.Value}");
+					}
+				}
+
+				//if (principal.Identity is ClaimsIdentity identity)
+				//{
+				//	var userId = identity.UniqueIdentifier ();
+
+
+				//}
+			}
+
+			if (string.IsNullOrEmpty (faceApiSubscription))
+			{
+				return req.CreateErrorResponse (HttpStatusCode.NotFound, "Unable to find Face token on server");
+			}
+
+			return req.CreateResponse (HttpStatusCode.OK, faceApiSubscription);
+		}
+	}
 }
---
+++ Agencies/Agencies.Functions/SecurityExtensions.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Security.Claims;
+using System.Security.Principal;
+
+namespace Agencies.Functions
+{
+	public static class SecurityExtensions
+	{
+		// https://github.com/Azure/azure-mobile-apps-net-server/wiki/Understanding-User-Ids
+		public static string UniqueIdentifier (this IPrincipal user)
+		{
+			if (user is ClaimsPrincipal principal)
+			{
+				if (principal.Identity is ClaimsIdentity identity)
+				{
+					return identity.UniqueIdentifier ();
+				}
+			}
+
+			return null;
+		}
+
+
+		public static string UniqueIdentifier (this ClaimsIdentity identity)
+		{
+			if (identity != null)
+			{
+				var stableSid = string.Empty;
+
+				var ver = identity.FindFirst ("ver")?.Value;
+
+				// the NameIdentifier claim is not stable.
+				if (string.Compare (ver, "3", StringComparison.OrdinalIgnoreCase) == 0)
+				{
+					// the NameIdentifier claim is not stable.
+					stableSid = identity.FindFirst ("stable_sid")?.Value;
+				}
+				else if (string.Compare (ver, "4", StringComparison.OrdinalIgnoreCase) == 0)
+				{
+					// the NameIdentifier claim is stable.
+					stableSid = identity.FindFirst (ClaimTypes.NameIdentifier).Value;
+				}
+
+				var provider = identity.FindFirst ("http://schemas.microsoft.com/identity/claims/identityprovider")?.Value;
+
+				if (string.IsNullOrEmpty (stableSid) || string.IsNullOrEmpty (provider))
+				{
+					return null;
+				}
+
+				return $"{provider}|{stableSid}";
+			}
+
+			return null;
+		}
+	}
+}
